a29baa5fafc15e31c2f15f46ff8d91cbfa37e6d8
Intuch.bu <intuch.bu@gmail.com>
2026-06-22 22:53:08 +0700

    refactor(HeroSection): update hero image format, modify text content, and adjust styles for improved presentation

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -1,34 +1,42 @@
-import heroImg from "@/assets/hero.png";
+import heroImg from "@/assets/hero.jpg";
 
 function HeroSection() {
   return (
     <section className="w-full">
       <div className="mx-auto flex max-w-[1366px] flex-col items-center justify-center gap-10 px-10 py-10 lg:flex-row lg:gap-8">
         <div className="max-w-sm text-right">
-          <h1 className="text-4xl font-bold leading-tight lg:text-5xl">
-            Stay Informed,
+          <h1 className="text-2xl font-bold leading-tight lg:text-3xl">
+            Seek Knowledge,
             <br />
-            Stay Inspired
+            Question Everything.
           </h1>
           <p className="mt-6 text-muted-foreground">
-            Discover a World of Knowledge at Your Fingertips. Your Daily Dose of
-            Inspiration and Information.
+            Development notes, competitive WH40k strategy, and lessons gathered
+            through experience.
           </p>
         </div>
 
-        <img className="w-[386px] h-[529px]" src={heroImg} alt="Hero" />
+        <img
+          className="w-[340px] h-[466px] rounded-lg"
+          src={heroImg}
+          alt="Hero"
+        />
 
         <div className="max-w-sm">
           <p className="text-xs text-muted-foreground">-Author</p>
-          <h2 className="mt-1 text-2xl font-bold">Thompson P.</h2>
+          <h2 className="mt-1 text-2xl font-bold">Intuch Bunluesup</h2>
           <p className="mt-4 text-muted-foreground">
-            I am a pet enthusiast and freelance writer who specializes in animal
-            behavior and care. With a deep love for cats, I enjoy sharing
-            insights on feline companionship and wellness.
+            Aspiring Full-Stack Developer <br /> Warhammer 40,000 Competitive
+            Player{" "}
+          </p>{" "}
+          <p className="mt-4 text-muted-foreground">
+            Building projects, learning continuously, and documenting the
+            journey along the way.
           </p>
           <p className="mt-4 text-muted-foreground">
-            When I&apos;m not writing, I spends time volunteering at my local
-            animal shelter, helping cats find loving homes.
+            Knowledge is earned through practice. This codex contains
+            development notes, competitive insights, and lessons learned along
+            the way.
           </p>
         </div>
       </div>
